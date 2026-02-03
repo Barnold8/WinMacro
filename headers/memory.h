f7b0c4ebafcd24a8a66fcb7979dd20e8a2940d72
Barnold88 <bandslover@outlook.com>
2026-02-03 01:12:53 +0000

    Fix memory issue

--- headers/memory.h
+++ headers/memory.h
@@ -2,13 +2,15 @@
 #define MEMORY_H
 
 
-#define da_append(xs,x) if (xs.count >= xs.capacity) {\
+#define da_append(xs,x)\
     do {\
-        if (xs.capacity == 0) xs.capcity = 256;\
-        else xs.capacity *= 2;\
-        xs.items = realloc(xs.items,xs.capacity*sizeof(xs.items))\
-    } while(0)\
-}
+        if (xs.count >= xs.capacity){\
+            if (xs.capacity == 0) xs.capacity = 256;\
+            else xs.capacity *= 2;\
+            xs.items = realloc(xs.items,xs.capacity*sizeof(xs.items));\
+        }\
+        xs.items[xs.count++] = x;\
+    } while(0)
 
 #endif
 
