145874b37808b12105bab96b4656d86778f2d800
Barnold88 <bandslover@outlook.com>
2026-02-06 00:00:26 +0000

    Define minimal implementation

---
+++ headers/hash_map.h
@@ -0,0 +1,16 @@
+#ifndef HASH_MAP
+#define HASH_MAP
+
+
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
+
+// TODO: define a struct like
+    // struct { float key; char value; } *hash = NULL;
+//  to define 
+
+struct {long int key; int value;} *hash = NULL; // need to decide what to use as the value type, for now int is just for viewing
+
+
+#endif
